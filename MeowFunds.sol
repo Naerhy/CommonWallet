3f52b7fc2d9822883a1eb25351c8058382cd1eb2
Sluia <sluia@protonmail.com>
2021-10-27 14:31:06 +0200

    some fixes

--- MeowFunds.sol
+++ MeowFunds.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 
 /*
-	- getter / setter for nbWhitelisted - [ ? ]
+	- get list of all WL addresses -> problem = public can get all DAO addresses
 
-	- add another condition for validID -> has to be >= 0 - [ ? ]
+	- onlyValidId modifier on some public view functions - [ ? ]
 
 */
 
@@ -14,79 +14,85 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract MeowFunds is Ownable {
 
 	struct S_Requests {
-		uint16 id;
+		uint id;
 		address applicant;
-		uint256 requestedAmount;
+		uint requestedAmount;
 		string message;
 		address[] approvals;
 		bool approved;
 	}
 
 	S_Requests[] private allRequests;
-	mapping(address => bool) private whitelistStatus;
-	uint8 private nbWhitelisted;
-	
-	event DepositFunds(address userAddress, uint256 amount);
-	event RequestWithdrawal(uint16 id, address applicant, uint256 requestedAmount, string message);
-	event ApproveRequest(uint16 id, address userAddress);
-	event WithdrawFunds(uint16 id, address to, uint256 amount);
+	mapping(address => bool) private whitelist;
+	uint private nbWhitelist;
 
-	modifier onlyWhitelisted() {
+	event SetWhitelistStatus(address userAddress, bool status);
+	event DepositFunds(address userAddress, uint amount);
+	event RequestWithdrawal(uint id, address applicant, uint requestedAmount, string message);
+	event ApproveRequest(uint id, address userAddress);
+	event WithdrawFunds(uint id, address to, uint amount);
+
+	modifier onlyWhitelist() {
 		require(getWhitelistStatus(_msgSender()) == true, "Address isn't whitelisted");
 		_;
 	}
 
-	modifier onlyValidId(uint16 id) {
-		require(id < allRequests.length && id >= 0, "Invalid ID");
+	modifier onlyValidId(uint id) {
+		require(id < allRequests.length && id >= 0, "Invalid request ID");
 		_;
 	}
 
 	function setWhitelistStatus(address userAddress, bool status) public onlyOwner {
 		require(getWhitelistStatus(userAddress) != status, "Unable to set the same status");
-		whitelistStatus[userAddress] = status;
+		whitelist[userAddress] = status;
 		if (status == true)
-			nbWhitelisted++;
+			nbWhitelist++;
 		else
-			nbWhitelisted--;
+			nbWhitelist--;
+		emit SetWhitelistStatus(userAddress, status);
 	}
 
 	function getWhitelistStatus(address userAddress) public view returns (bool) {
-		return whitelistStatus[userAddress];
+		return whitelist[userAddress];
 	}
 
-	function getContractBalance() private view returns (uint256) {
+	function getNbWhitelist() public view returns (uint) {
+		return nbWhitelist;
+	}
+
+	function getContractBalance() private view returns (uint) {
 		return address(this).balance;
 	}
 
-	function getRequestInformation(uint16 id) public view onlyWhitelisted onlyValidId(id) returns (uint16, address, uint256, string memory, address[] memory, bool) {
+	function getRequestInfo(uint id) public view returns (uint, address, uint, string memory, address[] memory, bool) {
 		return (allRequests[id].id, allRequests[id].applicant, allRequests[id].requestedAmount, allRequests[id].message, allRequests[id].approvals, allRequests[id].approved);
 	}
 
-	function getApprovalStatus(uint16 id) public view onlyWhitelisted returns (bool) {
-		if (allRequests[id].approvals.length > nbWhitelisted / 2)
+	function getApprovalStatus(uint id) public view returns (bool) {
+		if (allRequests[id].approvals.length > nbWhitelist / 2)
 			return (true);
 		return (false);
 	}
 
-	function depositFunds() payable public onlyWhitelisted {
+	function depositFunds() payable public onlyWhitelist {
 		require(msg.value > 0, "Unable to send 0 coin");
 		emit DepositFunds(_msgSender(), msg.value);
 	}
 
-	function requestWithdrawal(uint256 requestedAmount, string memory message) public onlyWhitelisted {
-		S_Requests memory newRequest = S_Requests(uint16(allRequests.length), _msgSender(), requestedAmount, message, new address[](0), false);
+	function requestWithdrawal(uint requestedAmount, string memory message) public onlyWhitelist {
+		S_Requests memory newRequest = S_Requests(allRequests.length, _msgSender(), requestedAmount, message, new address[](0), false);
 		allRequests.push(newRequest);
-		emit RequestWithdrawal(uint16(allRequests.length), _msgSender(), requestedAmount, message);
+		emit RequestWithdrawal(allRequests.length, _msgSender(), requestedAmount, message);
 	}
 
-	function approveRequest(uint16 id) public onlyWhitelisted onlyValidId(id) {
+	function approveRequest(uint id) public onlyWhitelist onlyValidId(id) {
 		require(_msgSender() != allRequests[id].applicant, "Applicant can't approve his own request");
 		require(checkAlreadyApproved(id, _msgSender()) == false, "This address has already approved");
 		allRequests[id].approvals.push(_msgSender());
 		emit ApproveRequest(id, _msgSender());
 	}
 
-	function withdrawFunds(uint16 id) public onlyWhitelisted onlyValidId(id) {
+	function withdrawFunds(uint id) public onlyWhitelist onlyValidId(id) {
 		require(allRequests[id].applicant == _msgSender(), "Invalid address");
 		require(getApprovalStatus(id) == true, "Not enough approvals");
 		require(getContractBalance() >= allRequests[id].requestedAmount, "Contract balance is too low");
@@ -95,11 +101,12 @@ contract MeowFunds is Ownable {
 		emit WithdrawFunds(id, _msgSender(), allRequests[id].requestedAmount);
 	}
 
-	function checkAlreadyApproved(uint16 id, address userAddress) private view returns (bool) {
-		for (uint256 i = 0; i < allRequests[id].approvals.length; i++) {
+	function checkAlreadyApproved(uint id, address userAddress) private view returns (bool) {
+		for (uint i = 0; i < allRequests[id].approvals.length; i++) {
 			if (allRequests[id].approvals[i] == userAddress)
 				return true;
 		}
 		return false;
 	}
+
 }
